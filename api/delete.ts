2591e66ffcbff3d6c38dfeaa07f598a96f198b63
Fer1196 <rodifer1196@hotmail.com>
2024-08-10 22:15:02 -0500

    Update delete.ts

--- api/delete.ts
+++ api/delete.ts
@@ -6,7 +6,7 @@ export default (req: VercelRequest, res: VercelResponse) => {
   if (req.method === "DELETE") {
     const { id } = req.query;
     // Lógica para borrar un recurso (ej. eliminar de una base de datos)
-    res.status(200).json({ message: "Recurso eliminado", id });
+    res.status(200).json({ message: "Recurso eliminado por Vercel", id });
   } else {
     res.status(405).json({ message: "Método no permitido" });
   }
