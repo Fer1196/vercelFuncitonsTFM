52799a86eefb3f0e34b0ae1232526f411b3f7a28
Fer1196 <rodifer1196@hotmail.com>
2024-08-11 01:04:41 -0500

    Update create.ts

--- api/create.ts
+++ api/create.ts
@@ -6,7 +6,7 @@ export default (req: VercelRequest, res: VercelResponse) => {
   if (req.method === "POST") {
     const data = req.body;
     // Lógica para crear un recurso (ej. guardar en una base de datos)
-    res.status(201).json({ message: "Recurso creado Vercellll", data });
+    res.status(201).json({ message: "Recurso creado Vercelll ffffl", data });
   } else {
     res.status(405).json({ message: "Método no permitido" });
   }
